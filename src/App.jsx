17031f8f1c5d8bb50cbbaeae9ed1d4fe5e33bd7f
AbdulSami\User <educationporposebyabdulsami@gmail.com>
2025-09-03 20:37:20 +0500

    PlaceHolder

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import Hero from "./assets/hero.png";
+import BackG from "./assets/bg.png";
+// c2099c26 API Key
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=c2099c26";
 function App() {
-  const [count, setCount] = useState(0)
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovie("spiderman");
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-full bg-[url(E:\ReactPracticeRevision\BasicReact\src\assets\bg.png)]">
+        {/* <img className="z-20" src={BackG} alt="BackGround" /> */}
+        <div className="w-full h-lvh flex flex-col">
+          <div className=" h-[70%]  flex place-content-center">
+            <img className=" w-auto h-auto" src={Hero} alt="" />
+          </div>
+          <div className="h-[30%]  flex flex-col items-center gap-10">
+            <h1 className="w-[50%] text-center text-white text-6xl  font-semibold">
+              Find{" "}
+              <span className="bg-gradient-to-r from-[#d6c7ff]  to-[#ab8bff] bg-clip-text text-transparent inline-block text-6xl font-bold">
+                Movies
+              </span>{" "}
+              You'll Enjoy Without the Hassle
+            </h1>
+            <div className="border-2 border-white">
+              <input
+                className=" text-white w-full h-[10%] p-10"
+                type="text"
+                placeholder="Search Movies Here!!"
+                value="SuperMan"
+                name=""
+                id=""
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
